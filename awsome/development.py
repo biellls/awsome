40459090bee5db36fb2772556ba091cb6dce0b38
gllobera <gllobera@hotelbeds.com>
2018-09-25 22:58:44 +0200

    Improved dry_run context manager to create buckets and renamed debug_environment to s3_sandbox

--- awsome/development.py
+++ awsome/development.py
@@ -1,14 +1,13 @@
-from contextlib import contextmanager
+from contextlib import contextmanager, ExitStack
 from typing import List
 import boto3
 from mock import patch
 from moto import mock_s3
-
-from awsome import s3, fakes
+from awsome import fakes
 
 
 @contextmanager
-def debug_environment(buckets: List[str]):
+def s3_sandbox(buckets: List[str]):
     with mock_s3():
         for bucket in buckets:
             conn = boto3.resource('s3', region_name='eu-west-1')
@@ -18,27 +17,21 @@ def debug_environment(buckets: List[str]):
 
 
 @contextmanager
-def dry_run():
-    with mock_s3():
-        ls_patcher = patch('awsome.s3.ls')
-        ls_mock = ls_patcher.start()
-        ls_mock.side_effect = fakes.fake_ls
+def dry_run(buckets: List[str]=(), patch_ls=True):
+    with mock_s3(), patch('awsome.s3.cp') as cp_mock, patch('awsome.s3.mv') as mv_mock, \
+            patch('awsome.s3.rm') as rm_mock, patch('awsome.s3.move_key') as move_key_mock:
+        for bucket in buckets:
+            conn = boto3.resource('s3', region_name='eu-west-1')
+            conn.create_bucket(Bucket=bucket)
 
-        cp_patcher = patch('awsome.s3.cp')
-        cp_mock = cp_patcher.start()
         cp_mock.side_effect = fakes.fake_cp
-
-        mv_patcher = patch('awsome.s3.mv')
-        mv_mock = mv_patcher.start()
         mv_mock.side_effect = fakes.fake_mv
-
-        rm_patcher = patch('awsome.s3.rm')
-        rm_mock = rm_patcher.start()
         rm_mock.side_effect = fakes.fake_rm
+        move_key_mock.side_effect = fakes.fake_move_key
 
-        yield
-
-        ls_patcher.stop()
-        cp_patcher.stop()
-        mv_patcher.stop()
-        rm_patcher.stop()
+        if patch_ls:
+            with patch('awsome.s3.ls') as ls_mock:
+                ls_mock.side_effect = fakes.fake_ls
+                yield
+        else:
+            yield
