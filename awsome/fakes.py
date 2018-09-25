ac6cdab91fd6187f0675c8043cc88e3b43aaa744
gllobera <gllobera@hotelbeds.com>
2018-09-25 22:59:23 +0200

    Added fake_move_key

--- awsome/fakes.py
+++ awsome/fakes.py
@@ -1,4 +1,3 @@
-
 def fake_ls(uri: str, recursive: bool=False, session=None):
     print(f"aws s3 ls {'--recursive ' if recursive else ''}{uri}")
 
@@ -13,3 +12,7 @@ def fake_mv(from_uri, to_uri, session=None):
 
 def fake_rm(uri, session=None):
     print(f"aws s3 rm {uri}")
+
+
+def fake_move_key(from_bucket, from_key, to_bucket, to_key=None, session=None):
+    print(f'aws s3 mv s3://{from_bucket}/{from_key} s3://{to_bucket}/{to_key or from_key}')
